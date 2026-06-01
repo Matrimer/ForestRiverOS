5886de4984c21d0b3738e1c27673453a8177d20c
Matrimer <mats.rimereit@mailbox.org>
2026-06-01 02:06:23 +0200

    V 0.0.1

---
+++ files/scripts/ensureautoupdates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# FROM WAYBLUE PROJECT
+
+set -oue pipefail
+
+systemctl enable rpm-ostreed-automatic.timer
+systemctl enable podman-auto-update.timer
+systemctl --global enable podman-auto-update.timer
+systemctl --global enable flatpak-user-update.timer
+systemctl enable flatpak-system-update.timer
---
+++ files/scripts/setearlyloading.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# FROM WAYBLUE PROJECT
+
+set -oue pipefail
+
+sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
